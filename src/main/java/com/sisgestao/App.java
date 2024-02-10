1e8dcbbf35f2de6848d088cf5a319adc84d1bf3e
Carlos André <andre.tjlba@gmail.com>
2024-02-09 22:03:23 -0300

    Iniciando projeto Java 21 dias

---
+++ src/main/java/com/sisgestao/App.java
@@ -0,0 +1,13 @@
+package com.sisgestao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
